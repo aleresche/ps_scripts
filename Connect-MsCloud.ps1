d2c031da5e823d2dfbf6e45a0999255ba56e7634
Arnaud Leresche <arnaud.leresche@b-i.com>
2016-11-29 10:30:32 +0100

    new versions

--- Connect-MsCloud.ps1
+++ Connect-MsCloud.ps1
@@ -20,30 +20,71 @@ Param(
     [Parameter(Position=0, Mandatory=$false, ValueFromPipeline=$true)]  
     [string] $Username,  
     [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)]  
-    [string] $Password,
-    [Parameter(Position=2, Mandatory=$true, ValueFromPipeline=$true)]
-    [string] $Domain  
+    [string] $Password
 )
 #Remove all existing Powershell sessions  
 Get-PSSession | Remove-PSSession
-
-#Did they provide creds?  If not, ask them for it. 
-if (([string]::IsNullOrEmpty($Username) -eq $false) -and ([string]::IsNullOrEmpty($Password) -eq $false)) 
-{ 
-    $SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
-    #Build credentials object  
-    $UserCredential  = New-Object System.Management.Automation.PSCredential $Username, $SecurePassword
-} 
-else 
-{ 
-    #Build credentials object
-    write-host "Requesting credential..."  -ForegroundColor Yellow
-    $UserCredential  = Get-Credential
+#Introduction
+write-host "Preparing Info to setup connection...`nLooking for login cache..." -ForegroundColor Yellow
+#Login Management
+$inputCred = Join-Path $PWD.ToString()"\..\Cred.xml"  
+if(![System.IO.File]::Exists($inputCred)){
+    # Connection to tenant - use this only 1st time to collect credentials
+    write-host "No Credential Found, creating cache..."
+    if (([string]::IsNullOrEmpty($Username) -eq $false) -and ([string]::IsNullOrEmpty($Password) -eq $false)) {
+        $SecurePassword = ConvertTo-SecureString -AsPlainText $Password -Force
+        #Build credentials object  
+        $inputCred  = New-Object System.Management.Automation.PSCredential $Username, $SecurePassword
+    }
+    else {
+         Get-Credential | Export-Clixml $inputCred
+    }
 }
+#load User Admin for display
+$AdmUsr = get-content ..\Cred.xml | select-string "UserName"
+$AdmUsr = $AdmUsr -replace '<S N="UserName">'; ''
+$AdmUsr = $AdmUsr -replace '</S>'; ''
+$Domain = $AdmUsr -split '@'
+# Set this variable to the location of the file where credentials are cached
+$UsrCredential = Import-Clixml $inputCred
 
-#Establishing Session
-write-host "Loading Online Session...." -ForegroundColor Yellow
-$Session = New-PSSession -Name "ExchangeOnline" -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
-Import-PSSession $Session -AllowClobber | Out-Null
-write-host "Online session established !" -ForegroundColor Yellow
+# Menu multiple choice to guide the user
+function Show-Menu {
+     Write-Host "================ Connecting ================" -ForegroundColor Yellow
+     Write-host "Connecting on Tenant : $($Domain[1])" -ForegroundColor Yellow
+     Write-Host "1: Press '1' Connect to O365 only" -ForegroundColor Yellow
+     Write-Host "2: Press '2' Connect to O365 & MS Online"  -ForegroundColor Yellow
+     Write-host "3: Press '3' Change admin login"
+     Write-Host "Q: Press 'Q' to quit." -ForegroundColor Yellow
+}
+do {
+     Show-Menu
+     write-host "Please make a selection" -ForegroundColor Yellow
+     $input = Read-Host
+     switch ($input)
+     {
+           '1' {write-host 'You chose option #1' -ForegroundColor Yellow} 
+           '2' {write-host 'You chose option #2' -ForegroundColor Yellow}
+           '3' {write-host 'You chose option #2' -ForegroundColor Yellow}
+           'q' {
+                #Cleaning sessions
+                write-host "Closing sessions...`nOperation aborted" -ForegroundColor Yellow
+                Get-PSSession | Remove-PSSession
+                exit
+               }
+     }
+}
+until ($input -eq 'q' -or $input -eq '1'-or $input -eq '2'-or $input -eq '3')
+
+if ($input -eq '2'){}
+
+#Connecting to Azure AD & Exchange Online
+write-host "Connecting using User : " $AdmUsr -ForegroundColor Yellow
+if ($input -eq '2'){
+    connect-msolservice -credential $UsrCredential
+}
+$Session = New-PSSession -Name "ExchangeOnline" -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UsrCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -AllowClobber |  out-null
+write-host "Connected !" -ForegroundColor Yellow
+
 
