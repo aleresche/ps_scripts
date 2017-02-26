df698ed3e25196d9fd9a23f36a666335ccd9494a
aleresche <arnaud.leresche@b-i.com>
2017-02-26 22:20:17 +0100

    new renaming script

---
+++ Rename-O365users.ps1
@@ -0,0 +1,30 @@
+<#
+.Synopsis
+   	O365 User Account Renaming Tool
+.DESCRIPTION
+   	Renaming tool for Office365 users, including :
+    - set primary smtp address
+    - set User principal name (UPN)
+    - set SIP address 
+    - retain existing emails as secondary
+
+    msol-service module needed to access azure AD
+.EXAMPLE
+	./Rename-O365users.ps1
+.NOTES
+   	Version 0.1 
+   	Written by Arnaud Leresche
+#>
+
+##############################################################################################################################################################
+# Renaming different Emails (including SIP)
+##############################################################################################################################################################
+Function renameEmails {
+
+}
+##############################################################################################################################################################
+# Renaming User Principal Name (UPN)
+##############################################################################################################################################################
+Function renameUPN {
+
+}
