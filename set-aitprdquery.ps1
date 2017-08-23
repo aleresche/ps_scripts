40a43fe6d351fe824398e0b244e387cbb2b5ace1
Arnaud Leresche <arnaud.leresche@b-i.com>
2017-08-23 08:56:26 +0200

    updates

---
+++ set-aitprdquery.ps1
@@ -0,0 +1,11 @@
+<#
+.Synopsis
+   	Powershell script to remote exec query on AIT HUB
+.DESCRIPTION
+   	allow push of SQL query into HUB Production Database 
+.EXAMPLE
+	./set-aitprdquery.ps1
+.NOTES
+    Version 0.1
+    Written by Arnaud Leresche
+#>
