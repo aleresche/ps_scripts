821f2cc21f71b56f7e1fa9d514c21185a65677e8
aleresche <arnaud.leresche@b-i.com>
2017-09-11 23:47:23 +0200

    selfsign update

--- make-aitselfsignedssl.ps1
+++ make-aitselfsignedssl.ps1
@@ -4,16 +4,26 @@
 .DESCRIPTION
    	Create Self sign using powershell module
 .EXAMPLE
-	./set-aitprdquery.ps1
+	./make-aitselfsignedssl.ps1 -certname -passwd -
 .NOTES
     Version 0.1
     Written by Arnaud Leresche
 #>
-$certname
+######################################################################################################################################################################################################
+#Parameters
+######################################################################################################################################################################################################
+Param(
+    [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]  
+    [string] $certname,
+    [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true)]  
+    [string] $passwd
+)
+# Creating CA Root
+$caroot = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname "AIT Local Certificate Authority" -KeyusageProperty All -KeyUsage CertSign, CRLSign, DigitalSiganture
+# Creating Client Cert
 $certpath = 'cert:\localmachine\my\'
-
-$cert = New-SelfSignedCertificate 
-$password = ConvertTo-SecureString -String 'Passw0rd!' -Force -AsPlainText
+$cert = New-SelfSignedCertificate -Subject "*.testing.local"  -DnsName "*.testing.local, testing.local" -CertStoreLocation $certpath  -Signer $caroot
+# Export it to PFX file
+$password = ConvertTo-SecureString -String $passwd -Force -AsPlainText
 $certpath = $path + $cert.Thumbprint
-
 Export-PfxCertificate -Cert $certpath -FilePath "$PWD.Path\$certname.pfx" -Password $password 
