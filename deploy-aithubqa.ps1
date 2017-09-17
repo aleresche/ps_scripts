68954fa208a0e78ede4103aaf5f0d62c3ee7d5f6
Arnaud Leresche <arnaud.leresche@b-i.com>
2017-09-17 22:09:54 +0200

    update deploy ait

--- deploy-aithubqa.ps1
+++ deploy-aithubqa.ps1
@@ -1,14 +1,20 @@
 <#
 .Synopsis
-   	AIT Hub Deploy
+   	   AIT Hub Deploy Script
 .DESCRIPTION
-   	pshell script that will deploy a new AIT HUB Release
+	   pshell script that will deploy a new AIT HUB Release, 
+	   credential are managed within the script and will be prompted when needed
+	   DB script are loaded with pshell SQLServer module
+	   the file will be managed through remote ps session and copy item
+	   a compare will be made between file in the release to file in destination and update if needed
+	   if conflict of version prompt will be made to ask admin for manual input about which version keep
+	   
 .EXAMPLE
-	./deploy-aithubqa.ps1 -releasepath c:\temp\hub.1.7.0\
+	   ./deploy-aithubqa.ps1 -releasepath c:\temp\hub.1.7.0\
 .NOTES
-   	Version 0.5
+   	   Version 0.2
        
-   	Written by Arnaud Leresche
+   	   Written by Arnaud Leresche
 #>
 #=========================================================================================================================================================================
 # Parameters 
@@ -20,14 +26,24 @@ Param(
 #=========================================================================================================================================================================
 
 #=========================================================================================================================================================================
+# Main Variables
+#=========================================================================================================================================================================
+# Servers Infos
+$dbServer = "172.18.223.191"
+$webServer = "10.1.1.1"
+# Path for release zip file
+$fullpath = "$PWD\$releasefile"
+$releaseVersion = $releasefile -replace ".zip",""
+$deployedrelease = $fullpath -replace ".zip",""
+# Credential 
+$inputCred = Join-Path $PWD.ToString()".\Cache_login.xml"
+#=========================================================================================================================================================================
+
+#=========================================================================================================================================================================
 # Init  
 #=========================================================================================================================================================================
 # Remove all existing Powershell sessions  
 Get-PSSession | Remove-PSSession
-
-# Variables
-$dbServer = "10.7.12.62"
-$webServer = "10.7.12.168"
 
 # Test if SQL pshell module is present, install it if not the case
 if ((Get-Command "Invoke-Sqlcmd") -eq $null) {
@@ -36,7 +52,7 @@ if ((Get-Command "Invoke-Sqlcmd") -eq $null) {
 }
 
 # Test if Release file exists
-if(![System.IO.File]::Exists($releasefile)){
+if(![System.IO.File]::Exists($fullpath)){
 	write-host "No Release Found !`n`nmake sure a release zip file is present and named HUB.x.x.x" -ForegroundColor Red
 	Exit;
 }
@@ -44,17 +60,18 @@ if(![System.IO.File]::Exists($releasefile)){
 #No cache found asking for Credential
 if(![System.IO.File]::Exists($inputCred)){
 	write-host "No Credential Found, creating cache..." -ForegroundColor Red
-	write-host "Please provide Credential :" -ForegroundColor Cyan
-    $inputCred = Join-Path $PWD.ToString()".\Cache_login.xml"  
+	write-host "Please provide Credential :" -ForegroundColor Cyan  
     Get-Credential | Export-Clixml $inputCred
 }
 # loading XML credential file
 $UsrCredential = Import-Clixml $inputCred
 
 # Uncompress Zip file for release
-$deployedrelease = $releasefile -replace ".zip",""
-Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::ExtractToDirectory($releasefile, $deployedrelease)
+if (![System.IO.File]::Exists($deployedrelease)){
+	write-host "Extracting release zip file....`nPlease wait" -ForegroundColor Magenta
+	Add-Type -assembly "system.io.compression.filesystem"
+	[io.compression.zipfile]::ExtractToDirectory($fullpath, $deployedrelease)
+}
 #=========================================================================================================================================================================
 #=========================================================================================================================================================================
 # Menu Deploy 
@@ -62,7 +79,7 @@ Add-Type -assembly "system.io.compression.filesystem"
 # 
 function Show-MenuConnect {
 	Write-Host "================ AIT Hub PsDeploy ================" -ForegroundColor Cyan
-	Write-host "AIT HUB version to Deploy : $deployedrelease" -ForegroundColor Cyan
+	Write-host "AIT HUB version to Deploy : $releaseVersion" -ForegroundColor Cyan
 	write-host "Production ENV Script" -ForegroundColor Cyan
 	Write-Host "=================== Options ======================" -ForegroundColor Cyan
 	Write-Host "1: Press '1' Deploy Databases" -ForegroundColor Cyan
@@ -72,7 +89,7 @@ function Show-MenuConnect {
 }
 do {
 	Show-MenuConnect
-	write-host "Please make a selection" -ForegroundColor Cyan
+	write-host "`nPlease make a selection" -ForegroundColor Cyan
 	$input = Read-Host
 	switch ($input)
 	{
@@ -81,7 +98,7 @@ do {
 		  '3' {write-host 'You chose option #3' -ForegroundColor Cyan}
 		  'q' {
 			   #Cleaning sessions
-			   write-host "Closing sessions...`nOperation aborted" -ForegroundColor Cyan
+			   write-host "Closing sessions..." -ForegroundColor Cyan
 			   Get-PSSession | Remove-PSSession
 			   exit
 			  }
@@ -102,7 +119,7 @@ if ($input -eq "1"){
 		Invoke-Sqlcmd -InputFile $deployedrelease"\01 - Database\Configuration\"$queryname`
 		-ServerInstance $dbServer -Username $UsrCredential.Username -Password $UsrCredential.GetNetworkCredential().password | Out-File -filePath $PWD.Path"\result_$queryname.rpt" 
 	}
-	write-host "SQL script deployed, you can check results in the rpt files"
+	write-host "SQL script deployed, you can check results in the rpt files" -ForegroundColor Magenta
 	Show-MenuConnect
 }
 
@@ -116,7 +133,7 @@ if ($input -eq "2"){
 	Get-ChildItem -Path $deployedrelease"\02 - Web services\" | ForEach-Object {
 		Copy-Item -FromSession $WebServerSession -Path $deployedrelease"\02 - Web services\"$_.Name -Destination "D:\Blue Infinity\Web Services\"$_.Name
 	}
-	write-host "Web Services deployed" -ForegroundColor Green
+	write-host "Web Services deployed" -ForegroundColor Magenta
 	Show-MenuConnect
 }
 #=========================================================================================================================================================================
@@ -129,13 +146,13 @@ if ($input -eq "3"){
 	Get-ChildItem -Path $deployedrelease"\03 - Web Content\" | ForEach-Object {
 		Copy-Item -FromSession $WebServerSession -Path $deployedrelease"\01 - Web services\"$_.Name -Destination "D:\inetpub\wwwroot\"$_.Name
 	}
-	write-host "Web Content deployed" -ForegroundColor Green
+	write-host "Web Content deployed" -ForegroundColor Magenta
 	Show-MenuConnect
 }
 #=========================================================================================================================================================================
 
 #=========================================================================================================================================================================
 # Cleaning sessions
-write-host "Deployment Completed" -ForegroundColor Green
+write-host "Deployment Completed" -ForegroundColor Cyan
 Get-PSSession | Remove-PSSession
 #=========================================================================================================================================================================
